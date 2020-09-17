9ca9bffa08a761800932e8d027be47c9f7e5f073
Ariel Reuven Kogan <ar.kogan@gmail.com>
2020-09-17 06:16:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -64,7 +64,6 @@ function placeOrder(cardNumber) {
   if (cardNumber === undefined) {
     return `Sorry, we don't have a credit card on file for you.`
   } else {
-    cart.splice(0, cart.length)
     return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
-  }
+  } return cart.splice(0, cart.length)
 }
